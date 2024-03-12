1ef8bf1ed25d934de34f6970f2efacfda0ce6be9
narendra <narendrasd5860@gmail.com>
2024-03-12 13:12:50 +0530

    first commit

--- key.tf
+++ key.tf
@@ -1,4 +1,15 @@
+# resource "aws_key_pair" "launch_tamplate" {
+#   key_name   = "deployer-key"
+#   public_key = file("${path.module}/rsa.pub")
+# }
+
+
 resource "aws_key_pair" "launch_tamplate" {
-  key_name   = "deployer-key"
-  public_key = file("${path.module}/rsa.pub")
+  key_name   = "my-keypair"  # Change this to your desired key pair name
+  public_key = tls_private_key.my_keypair.public_key_openssh
+}
+
+resource "tls_private_key" "my_keypair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
