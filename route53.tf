823c123eb325bdebd15dd8e2bcd2ba8b13d4b9a9
narendra <narendrasd5860@gmail.com>
2024-03-13 09:26:29 +0530

    first commit

--- route53.tf
+++ route53.tf
@@ -1,15 +1,14 @@
 resource "aws_route53_record" "workspace_record" {
-  zone_id = "Z0804944375KWXIC7X506"
+  zone_id = "Z0804944375KWXIC7X506"# add your route 53 zone id 
   name    = "infogyan.online"
   type    = "A"
-
+  
   alias {
     name                   = aws_lb.example.dns_name
     zone_id                = aws_lb.example.zone_id
     evaluate_target_health = true
   }
 }
-
 output "dns" {
   value = aws_lb.example.dns_name
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,6 @@
 region         = "ap-south-1"
 port           = [22, 443, 80, 3306]
-ami            = "ami-067c21fb1979f0b27"
+ami            = "ami-05205cb302b6dca6f"
 username       = "admin"
 password       = "admin3306"
 instance_type  = "t2.micro"
